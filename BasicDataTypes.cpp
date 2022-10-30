20ab82b3228c7c1e0076712867f6ace41361e8d6
Fikrii12 <nurulfikri756@gmail.com>
2022-10-30 13:10:09 +0700

    Soal pertama HackerRank

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int n1;
+    long n2;
+    char karakter;
+    float n3;
+    double n4;
+
+    cin>>n1>>n2>>karakter>>n3>>n4;
+    
+    printf("%d\n%ld\n%c\n%f\n%lf", n1, n2, karakter, n3, n4);
+}
